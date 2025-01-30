4033a8630d923d18510e80e51972f2cccff6674f
salman854raza <raza3213710@gmail.com>
2025-01-30 12:17:56 -0800

    updat hecktone.

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,26 +1,26 @@
-"use client"
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import CartSlice from './features/cartSlice'
-import storage from 'redux-persist/lib/storage';
-import persistReducer from 'redux-persist/lib/persistReducer';
+// "use client"
+// import { combineReducers, configureStore } from '@reduxjs/toolkit'
+// import CartSlice from './features/cartSlice'
+// import storage from 'redux-persist/lib/storage';
+// import persistReducer from 'redux-persist/lib/persistReducer';
 
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-}
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// }
 
-const reducer = combineReducers({
-  cart: CartSlice
-})
+// const reducer = combineReducers({
+//   cart: CartSlice
+// })
 
-const persistedReducer = persistReducer(persistConfig, reducer)
+// const persistedReducer = persistReducer(persistConfig, reducer)
 
-export const Store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({serializableCheck: false}),
-})
+// export const Store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({serializableCheck: false}),
+// })
 
-export type RootState = ReturnType<typeof Store.getState>
-export type AppDispatch = typeof Store.dispatch
+// export type RootState = ReturnType<typeof Store.getState>
+// export type AppDispatch = typeof Store.dispatch
